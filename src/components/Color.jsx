53c59358dab9d69646636697be1bcb165d604897
jawadkhanswati <jawadahmad5105555@gamil.com>
2025-01-21 01:35:04 +0500

    completed

---
+++ src/components/Color.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+// import '../index.css'
+// const Color = (props) => {
+//   return (
+//     <div>
+//       <div className='helo' style={{backgroundColor:props.color}}>{props.color}</div>
+//     </div>
+//   )
+// }
+
+// export default Color
